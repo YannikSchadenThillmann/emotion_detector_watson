e3c3fc186cb10b8b5dae873e6851416a575c98cb
YannikSchadenThillmann <y.schaden.thillmann@gmail.com>
2025-11-28 14:04:11 -0500

    Commit 3

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,17 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
+    status_code = response.status_code
+    if status_code == 400:
+        emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        return emotions
     response_json = json.loads(response.text)
     emotions = response_json["emotionPredictions"][0]["emotion"]
     max_emo = max(emotions, key=emotions.get)
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+''' server.py module '''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,22 +7,25 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    ''' renders index page '''
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    ''' use app on server '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
-
-    # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
-
+    dom_emo = response["dominant_emotion"]
+    if dom_emo is None:
+        return "Invalid text! Please try again!"
     # Return a formatted string with the sentiment label and score
     response_copy = dict(response)
     del response_copy["dominant_emotion"]
     response_str = str(response_copy).strip("}{")
-    dom_emo = response["dominant_emotion"]
-    return "For the given statement, the system response is {}. The dominant emotion is <b>{}</b>.".format(response_str, dom_emo)
+    message1 = f"For the given statement, the system response is {response_str}."
+    message2 = f"The dominant emotion is <b>{dom_emo}</b>."
+    return message1 + message2
 
 if __name__ == "__main__":
     app.run(debug = True)
